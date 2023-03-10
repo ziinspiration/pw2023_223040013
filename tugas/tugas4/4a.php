1bdbc20dc0def68662b767f7e8c506b4c222d1fa
ziinspiration <ilham.223040013@mail.unpas.ac.id>
2023-03-10 17:59:29 +0700

    Update

---
+++ tugas/tugas4/4a.php
@@ -0,0 +1,20 @@
+<?php
+$text = ["epan", "cipta", "akan", "u", "asa", "esok"];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4a</title>
+</head>
+
+<body>
+    <p>M<?= $text[4] ?> d<?= $text[0]  ?>mu di<?= $text[1] ?>kan oleh apa yang kau kerj<?= $text[2] ?> hari ini,
+        b<?= $text[3] ?>kan b<?= $text[5] ?></p>
+</body>
+
+</html>
